451da8282ea83c7202f4d9776aad43ed7773e805
Nurlan <bnk40191@gmail.com>
2018-01-30 04:32:01 +0300

    test

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(function(request, response) {
+    console.log(request.method, request.url);
+    const text = fs.readFileSync('index.html', 'utf-8');
+    response.end(text);
+});
+
+server.listen('3000');
+console.log('server started!');
